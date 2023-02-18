55b2b33aafc59811cffe2d9ecff17ba3f37148ec
Emma <emmanuelsalvocv@gmail.com>
2023-02-18 11:23:18 -0300

    Agregadas filas

--- helpers/googleSheetsQueries.js
+++ helpers/googleSheetsQueries.js
@@ -2,7 +2,7 @@ const googleSheetsQueries = async (sheets, auth) => {
 	const customersList = await sheets.spreadsheets.values.get({
 		spreadsheetId: '1drG4DcZhUWgygvmqSq8JGM-hYAVk2WchOkDMevevCl8',
 		auth,
-		range: 'Reservas!A2:N1000',
+		range: 'Reservas!A2:N51000',
 		valueRenderOption: 'FORMATTED_VALUE',
 	});
 
